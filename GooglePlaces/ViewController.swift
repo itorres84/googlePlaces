037e39b0e946fb6fd2f0525170382494355b93ab
Intercam <intercam@imac-de-intercam.local>
2017-05-26 14:06:27 -0500

    Initial Commit

---
+++ GooglePlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglePlaces
+//
+//  Created by Intercam on 26/05/17.
+//  Copyright © 2017 Intercam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
